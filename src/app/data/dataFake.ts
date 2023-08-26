115bc8e6a67cf7fdee00d4cb41ee97a267fe33de
Maria Eduarda Alves Gil da Costa <mariaeduardaa888@gmail.com>
2023-08-26 17:49:09 -0400

    'm'

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "DISNEY ANUNCIA NOVO JOGO",
+    "description": "O jogo DreamLight Valley Disney é o mais novo jogo anunciado pela empresa.",
+    "photoCover": "https://assets.nintendo.com/image/upload/c_limit,f_auto,h_300,q_auto,w_400/v1/ncom/software/switch/70010000033556/Video/posters/3_DDV_22429_V_Update3Trailer_A_01_ON01_NS_HD_1280x720_NOA_EN"
+  },
+  {
+    "id": "2",
+    "title": "ENCANTO TEM SUCESSO INESPERADO",
+    "description": "O filme recente Encanto teve um sucesso inesperado pelo público, o longa encantou os jovens com suas cores vibrantes e os adultos com suas músicas profundas.",
+    "photoCover": "https://s2-g1.glbimg.com/BLyABtiLVo3JpH2190vghbQ2tkM=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/T/S/FMXflBTgAEN8bhmmG5eg/encanto-group-1.jpg"
+  },
+  {
+    "id": "3",
+    "title": "NOVO FILME ANUNCIADO PELA DISNEY",
+    "description": "O novo filme da Disney aborda temas importantes através de uma inovadora forma de animação.0",
+    "photoCover": "https://www.leiagora.com.br/imgsite/entrete/amp-Ember-e-Wade-em-Elementos.jpg"
+  },
+  {
+    "id": "4",
+    "title": "QUIZ - Qual princesa você é?",
+    "description": "O filme recente Encanto teve um sucesso inesperado pelo público, o longa encantou os jovens com suas cores vibrantes e os adultos com suas músicas profundas.",
+    "photoCover": "https://img.quizur.com/f/img616355d960b245.46922144.jpg?lastEdited=1633899996"
+  },
+  {
+    "id": "5",
+    "title": "ENCANTO TEM SUCESSO INESPERADO",
+    "description": "O filme recente Encanto teve um sucesso inesperado pelo público, o longa encantou os jovens com suas cores vibrantes e os adultos com suas músicas profundas.",
+    "photoCover": "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/04/ordem-cronologica-filmes-da-marvel.jpg?w=876&h=484&crop=1"
+  }
+]
